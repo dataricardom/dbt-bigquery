f1e559ee9749aa4a2fb9d27a0af3553d492dd514
Ricardo Marques <dataricardom@gmail.com>
2025-08-01 16:00:17 -0300

    Corrigindo Consultas e Adicionando Campos

--- datawarehouse/models/intermediate/vendas/int_vendas_join_vendas_produtos_clientes.sql
+++ datawarehouse/models/intermediate/vendas/int_vendas_join_vendas_produtos_clientes.sql
@@ -30,7 +30,21 @@ int_join_clientes_produtos_vendas as (
         p.id_produto,
         p.produto_nome,
         p.categoria_produto,
-        p.preco_produto
+        p.preco_produto,
+        case
+            when c.estado_sigla in ('SP', 'RJ', 'MG', 'ES') then 'Sudeste'
+            when c.estado_sigla in ('RS', 'SC', 'PR') then 'Sul'
+            when c.estado_sigla in ('DF', 'GO', 'MT', 'MS') then 'Centro-Oeste'
+            when
+                c.estado_sigla in ('AM', 'RR', 'AP', 'PA', 'TO', 'RO', 'AC')
+                then 'Norte'
+            when
+                c.estado_sigla in (
+                    'MA', 'PI', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA'
+                )
+                then 'Nordeste'
+            else 'Região Desconhecida'
+        end as regiao
     from stg_vendas as v
     inner join stg_clientes as c on v.id_cliente = c.id_cliente
     inner join stg_produtos as p on v.id_produto = p.id_produto
